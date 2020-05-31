cc05ed1a520ead224c45255f6f97d380ab1ce561
iqFareez <60868965+fareezMaple@users.noreply.github.com>
2020-05-31 23:57:24 +0800

    Basic implementation of dark mode
    just scene

--- Brakeys - High score/Assets/Scripts/Dice.cs
+++ Brakeys - High score/Assets/Scripts/Dice.cs
@@ -7,21 +7,17 @@ public class Dice : MonoBehaviour
     
     public TextMeshProUGUI scoreText1;
     public TextMeshProUGUI scoreText2;
+    public TextMeshProUGUI totalText;
 
     private int number1;
     private int number2;
-
-    private void Start()
-    {
-        
-    }
 
     public void BrakeysRollDice()
     {
         int number = Random.Range(1, 7);
         scoreText.text = number.ToString();
 
-        Debug.Log("Called Brakeys code");
+        // Debug.Log("Called Brakeys code");
     }
 
     public void RollDiceTwice()
@@ -31,6 +27,8 @@ public class Dice : MonoBehaviour
 
         scoreText1.text = number1.ToString();
         scoreText2.text = number2.ToString();
+        totalText.text = "Total: " + (number1 + number2).ToString(); //total of this two number
+
     }
 
     public int getNum1 => number1;
