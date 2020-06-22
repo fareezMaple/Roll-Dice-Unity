87df7f71671245df36e46789714a48365cb88f61
iqFareez <60868965+fareezMaple@users.noreply.github.com>
2020-06-22 14:50:54 +0800

    keyboard shortcut

---
+++ Brakeys - High score/Assets/Scripts/KeyboardInput.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class KeyboardInput : MonoBehaviour
+{
+    public GameObject singleMode; //check if mode is on/off
+    public GameObject doubleMode;
+    public GameObject tooltipText;
+    
+    public Button rollSingle;
+    public Button rollDouble;
+    public Button settingBtn;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            if (singleMode.activeSelf)
+            {
+                rollSingle.onClick.Invoke();
+                
+            } else if (doubleMode.activeSelf)
+            {
+                rollDouble.onClick.Invoke();
+            }
+        } //roll button
+        
+        if (Input.GetKeyDown(KeyCode.S))
+            settingBtn.onClick.Invoke();
+
+        if (Input.GetKeyDown(KeyCode.Tab))
+        {
+            tooltipText.SetActive(!tooltipText.activeSelf);
+        }
+    }
+}
